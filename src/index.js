ea4a2db130c6970b936ff685e9d9f3988ac225ee
radmir-shkred <psitecton@gmail.com>
2020-05-29 18:23:20 +0300

    commit 3

--- src/index.js
+++ src/index.js
@@ -1,1 +1,28 @@
-console.log('hello');
+import {createStore} from "redux";
+import reducer from "./reducer";
+import {dec, inc, rnd} from "./actions";
+
+const store = createStore(reducer);
+
+document.getElementById('inc')
+    .addEventListener('click', () => {
+        store.dispatch(inc());
+    })
+
+document.getElementById('dec')
+    .addEventListener('click', () => {
+        store.dispatch(dec());
+    })
+
+document.getElementById('rnd')
+    .addEventListener('click', () => {
+        const payload = Math.floor(Math.random() * 9);
+        store.dispatch(rnd(payload));
+    })
+
+const update = () => {
+    document.getElementById('counter')
+        .innerHTML = store.getState();
+}
+
+store.subscribe(update);
